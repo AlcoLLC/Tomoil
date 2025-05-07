c6b892265ddd7b815497f3f16f298890e9a7ff4b
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2025-05-07 16:50:27 +0400

    root

--- core/settings.py
+++ core/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 if DEBUG == False:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 else:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
     
