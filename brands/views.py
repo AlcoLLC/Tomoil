4e6fcf58e2037d0780a431beb834d5a477907933
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2025-05-13 09:35:27 +0400

    brands

--- brands/views.py
+++ brands/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import get_object_or_404
 
 class BrandPromotionalListView(ListView):
     model = BrandPromotionalItem
-    template_name = 'brand_promotional_list.html'
+    template_name = 'brand_list.html'
     context_object_name = 'items'
 
 
