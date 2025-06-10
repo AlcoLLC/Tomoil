353355d1c7aeb5868c5edf1a3f9a83e4e4e5de22
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-06-10 09:53:45 +0400

    db

--- core/settings.py
+++ core/settings.py
@@ -195,7 +195,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from decouple import config
 
-
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
