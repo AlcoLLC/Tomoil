6201064c9a2ec5a69bec80722089d1f63e81013a
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-06-17 12:37:49 +0400

    brand-css

--- staticfiles/assets/js/home.js
+++ staticfiles/assets/js/home.js
@@ -1,44 +1,44 @@
-const swiper = new Swiper(".home-header .mySwiper", {
+const swiper = new Swiper('.home-header .mySwiper', {
   loop: true,
-  effect: "fade",
+  effect: 'fade',
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: ".home-header .swiper-button-next",
-    prevEl: ".home-header .swiper-button-prev",
+    nextEl: '.home-header .swiper-button-next',
+    prevEl: '.home-header .swiper-button-prev',
   },
   pagination: {
-    el: ".home-header .swiper-pagination",
+    el: '.home-header .swiper-pagination',
     clickable: true,
   },
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const commentSwiper = new Swiper(".comments-section .commentSwiper", {
+document.addEventListener('DOMContentLoaded', function () {
+  const commentSwiper = new Swiper('.comments-section .commentSwiper', {
     cssMode: true,
     slidesPerView: 2,
-    spaceBetween: 20,
+    spaceBetween: 10,
     navigation: {
-      nextEl: ".comments-section .swiper-button-next",
-      prevEl: ".comments-section .swiper-button-prev",
+      nextEl: '.comments-section .swiper-button-next',
+      prevEl: '.comments-section .swiper-button-prev',
     },
     pagination: {
-      el: ".comments-section .swiper-pagination",
+      el: '.comments-section .swiper-pagination',
     },
     mousewheel: true,
     keyboard: true,
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const rangeswiper = new Swiper(".product-range .mySwiper", {
-    slidesPerView: "auto",
+document.addEventListener('DOMContentLoaded', function () {
+  const rangeswiper = new Swiper('.product-range .mySwiper', {
+    slidesPerView: 'auto',
     spaceBetween: 30,
     navigation: {
-      nextEl: ".product-range .swiper-button-next",
-      prevEl: ".product-range .swiper-button-prev",
+      nextEl: '.product-range .swiper-button-next',
+      prevEl: '.product-range .swiper-button-prev',
     },
     autoplay: {
       delay: 5000,
@@ -50,86 +50,86 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const dropdowns = document.querySelectorAll(
-    ".cross-reference-content .dropdown"
+    '.cross-reference-content .dropdown'
   );
 
   dropdowns.forEach((dropdown) => {
-    const button = dropdown.querySelector(".dropdown-toggle");
+    const button = dropdown.querySelector('.dropdown-toggle');
 
-    button.addEventListener("click", (e) => {
+    button.addEventListener('click', (e) => {
       e.stopPropagation();
       dropdowns.forEach((d) => {
-        if (d !== dropdown) d.classList.remove("open");
+        if (d !== dropdown) d.classList.remove('open');
       });
-      dropdown.classList.toggle("open");
+      dropdown.classList.toggle('open');
     });
   });
 
-  document.addEventListener("click", () => {
-    dropdowns.forEach((d) => d.classList.remove("open"));
+  document.addEventListener('click', () => {
+    dropdowns.forEach((d) => d.classList.remove('open'));
   });
 });
 
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar");
-  const nav = document.querySelector("nav");
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  const nav = document.querySelector('nav');
 
   if (window.scrollY > 20) {
-    navbar.classList.remove("transparent");
-    navbar.classList.add("scrolled");
-    nav.classList.add("scrolled-nav");
+    navbar.classList.remove('transparent');
+    navbar.classList.add('scrolled');
+    nav.classList.add('scrolled-nav');
   } else {
-    navbar.classList.add("transparent");
-    navbar.classList.remove("scrolled");
-    nav.classList.remove("scrolled-nav");
+    navbar.classList.add('transparent');
+    navbar.classList.remove('scrolled');
+    nav.classList.remove('scrolled-nav');
   }
 
-  const headerWhatsapp = document.querySelector(".header-whatsapp");
-  const fixedWhatsapp = document.querySelector(".fixed-whatsapp");
+  const headerWhatsapp = document.querySelector('.header-whatsapp');
+  const fixedWhatsapp = document.querySelector('.fixed-whatsapp');
   const scrollPosition = window.scrollY;
   const headerHeight = document.querySelector(
-    ".placeholder-header"
+    '.placeholder-header'
   ).offsetHeight;
 
   if (scrollPosition > 100) {
-    fixedWhatsapp.style.opacity = "1";
-    fixedWhatsapp.style.visibility = "visible";
+    fixedWhatsapp.style.opacity = '1';
+    fixedWhatsapp.style.visibility = 'visible';
 
     if (headerWhatsapp) {
-      headerWhatsapp.style.opacity = "0";
-      headerWhatsapp.style.visibility = "hidden";
+      headerWhatsapp.style.opacity = '0';
+      headerWhatsapp.style.visibility = 'hidden';
     }
   } else {
-    fixedWhatsapp.style.opacity = "0";
-    fixedWhatsapp.style.visibility = "hidden";
+    fixedWhatsapp.style.opacity = '0';
+    fixedWhatsapp.style.visibility = 'hidden';
 
     if (headerWhatsapp) {
-      headerWhatsapp.style.opacity = "1";
-      headerWhatsapp.style.visibility = "visible";
+      headerWhatsapp.style.opacity = '1';
+      headerWhatsapp.style.visibility = 'visible';
     }
   }
 });
 
 function copyToClipboard(element) {
-  const url = element.getAttribute("data-url");
+  const url = element.getAttribute('data-url');
   const fullUrl = window.location.origin + url;
 
   navigator.clipboard
     .writeText(fullUrl)
     .then(() => {
-      const cardContent = element.closest(".card-content");
-      const messageDiv = cardContent.querySelector(".copy-message");
+      const cardContent = element.closest('.card-content');
+      const messageDiv = cardContent.querySelector('.copy-message');
 
       if (messageDiv) {
-        messageDiv.classList.add("show");
+        messageDiv.classList.add('show');
         setTimeout(() => {
-          messageDiv.classList.remove("show");
+          messageDiv.classList.remove('show');
         }, 2000);
       }
     })
     .catch((err) => {
-      console.error("Could not copy text: ", err);
+      console.error('Could not copy text: ', err);
     });
 }
