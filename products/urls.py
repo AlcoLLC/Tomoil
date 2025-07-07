802d77e1d8388b216ad245818f97d2ff539431fe
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-07-07 17:22:04 +0400

    corrections

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+from .views import pds_sds_view, all_data_sheets_view, products_view, products_detail_view
 
+app_name = 'products'
 
-
-
-
+urlpatterns = [
+    path("pds-sds/", pds_sds_view, name='pds_sds'),
+    path("all-data-sheets/", all_data_sheets_view, name='all_data_sheets'),
+    path("products/", products_view, name='products_list'),
+    path("product/<slug:product_slug>/", products_detail_view, name='products_detail_view'),
+]
