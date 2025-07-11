6fb86ba4fc32ff29c9e21306f8844b63994d9206
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-07-11 15:55:29 +0400

    home-vector

--- products/urls.py
+++ products/urls.py
@@ -9,22 +9,5 @@ urlpatterns = [
     path("products/", products_view, name='products_list'),
     path("product/<slug:product_slug>/", products_detail_view, name='products_detail_view'),
     path("products/product-range/<slug:product_range_slug>/", products_view, name='products_by_range'),
-    
-    # Friendly URLs for application areas
     path("products/application-area/<slug:application_area_slug>/", products_view, name='products_by_application'),
-    
-    # Friendly URLs for specifications
-    path("products/specification/<slug:specification_slug>/", products_view, name='products_by_specification'),
-    
-    # Friendly URLs for viscosities
-    path("products/viscosity/<slug:viscosity_slug>/", products_view, name='products_by_viscosity'),
-    
-    # Friendly URLs for compositions
-    path("products/composition/<slug:composition_slug>/", products_view, name='products_by_composition'),
-    
-    # Friendly URLs for pack sizes
-    path("products/pack-size/<slug:pack_size_slug>/", products_view, name='products_by_pack_size'),
-    
-    # Product detail
-    path("product/<slug:product_slug>/", products_detail_view, name='products_detail_view'),
 ]
